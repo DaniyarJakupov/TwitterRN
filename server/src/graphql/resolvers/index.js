cf9785ecc25cfefd5dd56fc7298b3d1614730621
DaniyarJakupov <ddr199305@gmail.com>
2017-11-24 11:08:53 +0100

    Add signUp mutation to index resolver

--- server/src/graphql/resolvers/index.js
+++ server/src/graphql/resolvers/index.js
@@ -1,6 +1,7 @@
 /* @flow */
 import GraphQLDate from 'graphql-date';
 import TweetResolvers from './tweet-resolver';
+import UserResolver from './user-resolver';
 
 export default {
   Date: GraphQLDate,
@@ -12,5 +13,6 @@ export default {
     createTweet: TweetResolvers.createTweet,
     updateTweet: TweetResolvers.updateTweet,
     deleteTweet: TweetResolvers.deleteTweet,
+    signUp: UserResolver.signup,
   },
 };
