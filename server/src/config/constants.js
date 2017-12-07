291929ef824e7b1b11974390d88476aa60552f0e
DaniyarJakupov <ddr199305@gmail.com>
2017-12-07 17:03:49 +0100

    Add JWT_SECRET

--- server/src/config/constants.js
+++ server/src/config/constants.js
@@ -2,6 +2,7 @@
 
 export default {
   PORT: process.env.PORT || 3000,
-  DB_URL: "mongodb://localhost/tweeter-development",
-  GRAPHQL_PATH: "/graphql"
+  DB_URL: 'mongodb://localhost/tweeter-development',
+  GRAPHQL_PATH: '/graphql',
+  JWT_SECRET: 'twisecret314',
 };
