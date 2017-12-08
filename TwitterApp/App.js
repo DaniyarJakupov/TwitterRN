d53a8418e501ef28c5a44f8243f359ea128c210c
DaniyarJakupov <ddr199305@gmail.com>
2017-12-08 16:05:36 +0100

    Setup redux && react-apollo

--- TwitterApp/App.js
+++ TwitterApp/App.js
@@ -1,40 +1,26 @@
 // @flow
-
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { UIManager } from 'react-native';
+import { ApolloProvider } from 'react-apollo';
+import { ThemeProvider } from 'styled-components';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu'
-});
+import { store, client } from './src/redux/store';
+import { colors } from './src/utils/constants';
+
+import Welcome from './src/components/Welcome';
+
+if (UIManager.setLayoutAnimationEnabledExperimental) {
+  UIManager.setLayoutAnimationEnabledExperimental(true);
+}
 
 export default class App extends Component<{}> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <ApolloProvider store={store} client={client}>
+        <ThemeProvider theme={colors}>
+          <Welcome />
+        </ThemeProvider>
+      </ApolloProvider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF'
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5
-  }
-});
---
+++ TwitterApp/src/redux/reducers/index.js
@@ -0,0 +1,7 @@
+// @flow
+import { combineReducers } from 'redux';
+
+export default client =>
+  combineReducers({
+    apollo: client.reducer(),
+  });
