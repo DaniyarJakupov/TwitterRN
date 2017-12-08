8f82d1d8ec4ec97f00320cffe7edfea250c91325
DaniyarJakupov <ddr199305@gmail.com>
2017-12-08 09:27:12 +0100

    Add auth check for tweet resolvers

--- server/src/graphql/resolvers/tweet-resolver.js
+++ server/src/graphql/resolvers/tweet-resolver.js
@@ -1,19 +1,52 @@
 /* @flow */
 import Tweet from '../../models/Tweet';
+import { requireAuth } from '../../services/auth';
 
 export default {
-  getTweets: () => Tweet.find({}).sort({ createdAt: -1 }),
-  getTweet: (_, { _id }) => Tweet.findById(_id),
-  createTweet: (_, args) => Tweet.create(args),
-  updateTweet: (_, { _id, ...rest }) => Tweet.findByIdAndUpdate(_id, rest, { new: true }),
+  getTweet: async (_, { _id }, { user }) => {
+    try {
+      await requireAuth(user);
+      return Tweet.findById(_id);
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  getTweets: async (_, args, { user }) => {
+    try {
+      await requireAuth(user);
+      return Tweet.find({}).sort({ createdAt: -1 });
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  createTweet: async (_, args, { user }) => {
+    try {
+      await requireAuth(user);
+      return Tweet.create(args);
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  updateTweet: async (_, { _id, ...rest }, { user }) => {
+    try {
+      await requireAuth(user);
+      return Tweet.findByIdAndUpdate(_id, rest, { new: true });
+    } catch (error) {
+      throw error;
+    }
+  },
+
   deleteTweet: async (_, { _id }) => {
     try {
       await Tweet.findByIdAndRemove(_id);
       return {
         message: 'Tweet was deleted!',
       };
-    } catch (err) {
-      throw err;
+    } catch (error) {
+      throw error;
     }
   },
 };
