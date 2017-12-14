071754f0da28241a78383db61cf6c3ef3a917964
DaniyarJakupov <ddr199305@gmail.com>
2017-12-14 17:44:52 +0100

    Add @flow

--- TwitterApp/src/screens/HomeScreen.js
+++ TwitterApp/src/screens/HomeScreen.js
@@ -1,31 +1,43 @@
+// @flow
 import React, { Component } from 'react';
 import styled from 'styled-components/native';
+import { graphql } from 'react-apollo';
+import { ActivityIndicator, FlatList } from 'react-native';
 
 import FeedCard from '../components/FeedCards/FeedCard';
+
+import GET_TWEETS_QUERY from '../graphql/queries/getTweets';
 
 const Wrapper = styled.View`
   flex: 1;
   padding-top: 5;
 `;
 
-const ScrollView = styled.ScrollView``;
-
 class HomeScreen extends Component {
   state = {};
+
+  _renderItem = ({ item }) => <FeedCard {...item} />;
+
   render() {
+    const { data } = this.props;
+    if (data.loading) {
+      return (
+        <Wrapper>
+          <ActivityIndicator size="large" />
+        </Wrapper>
+      );
+    }
     return (
       <Wrapper>
-        <ScrollView>
-          <FeedCard />
-          <FeedCard />
-          <FeedCard />
-          <FeedCard />
-          <FeedCard />
-          <FeedCard />
-        </ScrollView>
+        <FlatList
+          contentContainerStyle={{ alignSelf: 'stretch' }}
+          data={data.getTweets}
+          keyExtractor={item => item._id}
+          renderItem={this._renderItem}
+        />
       </Wrapper>
     );
   }
 }
 
-export default HomeScreen;
+export default graphql(GET_TWEETS_QUERY)(HomeScreen);
