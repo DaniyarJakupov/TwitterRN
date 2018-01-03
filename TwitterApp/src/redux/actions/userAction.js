7f3f4544b9c3c4bc50fa0aec14c7f416bdabc2b3
DaniyarJakupov <ddr199305@gmail.com>
2018-01-03 16:20:02 +0100

    Add userLogin action

---
+++ TwitterApp/src/redux/actions/userAction.js
@@ -0,0 +1,3 @@
+import { LOGIN_SUCCESS } from './types';
+
+export const userLogin = () => ({ type: LOGIN_SUCCESS });
--- TwitterApp/src/redux/reducers/userReducer.js
+++ TwitterApp/src/redux/reducers/userReducer.js
@@ -1,12 +1,15 @@
 // @flow
+import { LOGIN_SUCCESS } from '../actions/types';
+
 const initState = {
-  token: null,
   isAuth: false,
   info: null,
 };
 
 export default (state = initState, action) => {
   switch (action.type) {
+    case LOGIN_SUCCESS:
+      return { ...state, isAuth: true };
     default:
       return state;
   }
