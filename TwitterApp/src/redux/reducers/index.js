92a1242ed33aabf0e0ffe63be1a1d0177a8fc9f0
DaniyarJakupov <ddr199305@gmail.com>
2018-01-03 11:12:59 +0100

    Add userReducer

--- TwitterApp/src/redux/reducers/index.js
+++ TwitterApp/src/redux/reducers/index.js
@@ -1,9 +1,12 @@
 // @flow
 import { combineReducers } from 'redux';
+
 import nav from './navigationReducer';
+import user from './userReducer';
 
 export default client =>
   combineReducers({
     apollo: client.reducer(),
     nav,
+    user,
   });
---
+++ TwitterApp/src/redux/reducers/userReducer.js
@@ -0,0 +1,12 @@
+const initState = {
+  token: null,
+  isAuth: false,
+  info: null,
+};
+
+export default (state = initState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
