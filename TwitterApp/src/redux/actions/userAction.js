61063d6057891a9d5b4adaf17b8696d52f2e850e
DaniyarJakupov <ddr199305@gmail.com>
2018-01-04 11:29:47 +0100

    Add getUserInfo action

--- TwitterApp/src/redux/actions/userAction.js
+++ TwitterApp/src/redux/actions/userAction.js
@@ -1,3 +1,5 @@
-import { LOGIN_SUCCESS } from './types';
+import { LOGIN_SUCCESS, GET_USER_INFO } from './types';
 
 export const userLogin = () => ({ type: LOGIN_SUCCESS });
+
+export const getUserInfo = info => ({ type: GET_USER_INFO, payload: info });
--- TwitterApp/src/redux/reducers/userReducer.js
+++ TwitterApp/src/redux/reducers/userReducer.js
@@ -1,5 +1,5 @@
 // @flow
-import { LOGIN_SUCCESS } from '../actions/types';
+import { LOGIN_SUCCESS, GET_USER_INFO } from '../actions/types';
 
 const initState = {
   isAuth: false,
@@ -10,6 +10,8 @@ export default (state = initState, action) => {
   switch (action.type) {
     case LOGIN_SUCCESS:
       return { ...state, isAuth: true };
+    case GET_USER_INFO:
+      return { ...state, info: action.payload };
     default:
       return state;
   }
