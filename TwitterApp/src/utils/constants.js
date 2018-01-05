2171545edc4ea12648cfb10fcbfdf0f7f1b3a1d5
DaniyarJakupov <ddr199305@gmail.com>
2018-01-05 23:33:51 +0100

    Add avatars array

--- TwitterApp/src/utils/constants.js
+++ TwitterApp/src/utils/constants.js
@@ -6,4 +6,10 @@ export const colors = {
   LIGHT_GRAY: '#CAD0D6',
 };
 
-export const avatar = 'https://pbs.twimg.com/profile_images/932979502224953344/GSSBn8wF_400x400.jpg';
+export const avatars = [
+  { uri: 'https://i.imgur.com/IOZr9hJ.jpg' },
+  { uri: 'https://i.imgur.com/QsWdKbw.jpg' },
+  { uri: 'https://i.imgur.com/zpI6gTZ.jpg' },
+  { uri: 'https://i.imgur.com/N74761d.jpg' },
+  { uri: 'https://i.imgur.com/9dzAqU1.jpg' },
+];
