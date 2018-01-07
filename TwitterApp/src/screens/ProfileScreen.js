c26348a6e5b4ecc03ae013ff80e0028d45cb2eb9
DaniyarJakupov <ddr199305@gmail.com>
2018-01-07 16:22:21 +0100

    Style ProfileScreen

--- TwitterApp/src/screens/ProfileScreen.js
+++ TwitterApp/src/screens/ProfileScreen.js
@@ -1,20 +1,61 @@
 // @flow
 import React, { Component } from 'react';
+import { FlatList } from 'react-native';
 import styled from 'styled-components/native';
+import { connect } from 'react-redux';
+import { graphql, compose } from 'react-apollo';
 
-const Root = styled.View``;
+import ProfileHeader from '../components/ProfileHeader';
+import FeedCard from '../components/FeedCards/FeedCard';
 
-const T = styled.Text``;
+import GET_USER_TWEETS from '../graphql/queries/getUserTweets';
+
+const Root = styled.View`
+  flex: 1;
+`;
+const TweetFeed = styled.View`
+  flex: 1;
+  margin-top: 10;
+`;
 
 class ProfileScreen extends Component {
   state = {};
+
+  _renderItem = ({ item }) => <FeedCard {...item} />;
+
+  _renderPlaceholder = () => <FeedCard placeholder isLoaded={this.props.data.loading} />;
+
   render() {
+    const { info, data } = this.props;
+
     return (
       <Root>
-        <T>Profile</T>
+        <ProfileHeader {...info} />
+
+        <TweetFeed>
+          {data.loading ? (
+            <FlatList
+              data={[1, 2, 3]}
+              renderItem={this._renderPlaceholder}
+              keyExtractor={item => item}
+              contentContainerStyle={{ alignSelf: 'stretch' }}
+            />
+          ) : (
+            <FlatList
+              data={data.getUserTweets}
+              renderItem={this._renderItem}
+              keyExtractor={item => item._id}
+              contentContainerStyle={{ alignSelf: 'stretch' }}
+            />
+          )}
+        </TweetFeed>
       </Root>
     );
   }
 }
 
-export default ProfileScreen;
+const mapStateToProps = state => ({
+  info: state.user.info,
+});
+
+export default compose(graphql(GET_USER_TWEETS), connect(mapStateToProps))(ProfileScreen);
