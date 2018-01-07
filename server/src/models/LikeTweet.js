183c7cc0d8a12c07e0640efc2a9afc89b3e5d2ad
DaniyarJakupov <ddr199305@gmail.com>
2018-01-07 13:24:46 +0100

    Publish data to pubsub when userLikedTweet method is called

--- server/src/models/LikeTweet.js
+++ server/src/models/LikeTweet.js
@@ -2,6 +2,9 @@
 import mongoose, { Schema } from 'mongoose';
 import Tweet from './Tweet';
 
+import { pubsub } from '../config/pubsub';
+import { TWEET_LIKED } from '../graphql/resolvers/tweet-resolver';
+
 const LikeTweetSchema = new Schema({
   userId: {
     type: Schema.Types.ObjectId,
@@ -26,6 +29,8 @@ LikeTweetSchema.methods = {
       const tweet = await Tweet.decrementLikeCount(tweetId);
       const t = tweet.toJSON();
 
+      pubsub.publish(TWEET_LIKED, { [TWEET_LIKED]: { ...t } });
+
       return {
         isLiked: false,
         ...t,
@@ -37,6 +42,8 @@ LikeTweetSchema.methods = {
 
     this.tweets.push(tweetId); // add tweet to LikeTweet collection
     await this.save();
+
+    pubsub.publish(TWEET_LIKED, { [TWEET_LIKED]: { ...t } });
 
     return {
       isLiked: true,
